5782a7295af9643d187cfdcb04f2d652ab7ea3bd
IshaniNakarani12 <ishaninakarani12@gmail.com>
2024-04-15 11:38:42 +1000

    add files

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+console.log(__dirname);
+
+app.get("/register", (req, res) => {
+  //   res.send(`<h1>Register Page </h1>`);
+  res.sendFile(__dirname + "/public/register.html");
+});
 
 app.get("/", (req, res) => {
-  console.log("server received the req");
-  res.send("<h1>Info coming from the server</h1>");
+  //   console.log("server received the req");
+  //   res.send(`<h1>Home Page</h1>
+  //   <a href="/register">
+  //       <button>Register</button>
+  //   </a>
+  //   <a href="/login">
+  //       <button>Login</button>
+  //   </a>`);
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(8000, (error) => {
